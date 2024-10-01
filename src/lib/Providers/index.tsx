ef2346aa41f24f98e0bc71d433c06139c3de7bca
Ekramul <mdekramulhassan168@gmail.com>
2024-10-01 20:34:12 +0600

    add tanstaQuery

--- src/lib/Providers/index.tsx
+++ src/lib/Providers/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/order */
 "use client";
 
 import * as React from "react";
@@ -5,18 +6,28 @@ import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+import { Toaster } from "sonner";
+import UserProvider from "@/src/context/user.provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export interface ProvidersProps {
   children: React.ReactNode;
   themeProps?: ThemeProviderProps;
 }
 
+const queryClient = new QueryClient();
+
 export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
   return (
-    <NextUIProvider navigate={router.push}>
-      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <NextUIProvider navigate={router.push}>
+          <Toaster />
+          <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+        </NextUIProvider>
+      </UserProvider>
+    </QueryClientProvider>
   );
 }
