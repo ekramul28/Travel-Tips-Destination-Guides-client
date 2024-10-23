57c7a2cc3a6c4dd6f3163f495db3c06e05f844ea
Ekramul <mdekramulhassan168@gmail.com>
2024-10-23 15:12:20 +0600

    middleware added

--- src/app/(WithCommonLayout)/(home)/page.tsx
+++ src/app/(WithCommonLayout)/(home)/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import { Navbar, NavbarContent, NavbarItem } from "@nextui-org/navbar";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import React from "react";
@@ -28,13 +26,13 @@ const Sidebar = () => {
   ];
 
   return (
-    <div className=" fixed">
+    <div className="fixed">
       <div className="flex flex-col  items-start ">
         {links.map((link) => (
           <Link href={link.href} key={link.href}>
             <div
               className={`flex gap-2   items-start mt-8 ${
-                isActive(link.href) ? "text-gray-400" : "text-black"
+                isActive(link.href) ? "text-gray-300" : "text-slate-700"
               }`}
             >
               <link.icon size={30} />
