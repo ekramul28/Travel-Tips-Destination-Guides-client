a898f0ef58d8b7a7775871d9ceee4f7db40b1031
Ekramul <mdekramulhassan168@gmail.com>
2024-11-03 10:44:40 +0600

    some code added

--- src/app/(WithCommonLayout)/(home)/@usersId/page.tsx
+++ src/app/(WithCommonLayout)/(home)/@usersId/page.tsx
@@ -2,12 +2,11 @@
 import React from "react";
 
 import SuggestedUser from "@/src/components/Ui/SuggestedUser/SuggestedUser";
-import { IUser } from "@/src/types";
 import { getAllUser } from "@/src/services/user";
+import { IUser } from "@/src/types";
 
 const usersId = async () => {
   const allUser = await getAllUser();
-  // const allUser = { data: [] };
 
   return (
     <div>
